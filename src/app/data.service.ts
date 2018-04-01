ec500759f9b367239c0ac6b23cf6e2f88e2d8081
Joshua Styles <styles@outlook.com>
2018-04-01 22:45:48 +0100

    DataService
    Using DataService to share data between components.

---
+++ src/app/data.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
+@Injectable()
+
+export class DataService {
+
+  private goals = new BehaviorSubject<any>(['The initial goal', 'Another silly life goal', 'To learn Angular']);
+  
+  goal = this.goals.asObservable();
+  constructor() { }
+  
+  changeGoal(goal){
+  
+    this.goals.next(goal);
+  }
+
+}
